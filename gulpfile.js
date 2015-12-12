d68e0c5fd45b7ac2f9ce22c50f89cfcdaafcca86
Jimmy Hillis <jimmy@hillis.me>
2015-12-12 14:29:19 +0800

    Prepared module for NPM publish

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('es2015', function () {
+  gulp.src('src/bin/**/*.js')
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(gulp.dest('bin'));
+  gulp.src(['src/**/*.js', '!src/bin/**/*.js'])
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('default', ['es2015']);
